b53a3ccc22e3367b10d87405a393adac8f8ecea1
Florent BEZARA <bezaraflorent@gmail.com>
2025-04-02 10:28:25 +0300

    Mise à jour de l'URL d'origine autorisée dans la configuration CORS pour le développement local

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-   'allowed_origins' => [env('FRONTEND_URL', 'https://me.gasycoder.com')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://127.0.0.1:5173')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
