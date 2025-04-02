a349109108df664a69e25291e80ab4216f077cc3
bezara <bezaraflorent@gmai.com>
2025-04-02 08:36:17 +0200

    Mise à jour de config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -17,16 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-<<<<<<< HEAD
-
-    'allowed_origins' => [env('FRONTEND_URL', 'https://me.gasycoder.com')],
-
-=======
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-    ],
->>>>>>> 7f966c46ec96e011652ff055ca521930e3ede89a
+   'allowed_origins' => [env('FRONTEND_URL', 'https://me.gasycoder.com')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
